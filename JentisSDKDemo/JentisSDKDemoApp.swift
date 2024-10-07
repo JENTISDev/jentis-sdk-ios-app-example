360cdd0a2a822254343e5ea4e7adbebffe79f4a0
Alexandre Oliveira <alexandre.soli@gmail.com>
2024-10-07 14:49:53 +0100

    initial config

---
+++ JentisSDKDemo/JentisSDKDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JentisSDKDemoApp.swift
+//  JentisSDKDemo
+//
+//  Created by Alexandre Oliveira on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JentisSDKDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+        }
+    }
+}
